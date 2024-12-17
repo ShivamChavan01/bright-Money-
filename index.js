00df4432ea0dedc7db1834f6219a249c52ef960d
Shivam <shivamrc189@gmail.com>
2024-12-17 11:33:08 +0530

    a

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
-const draggable = document.getElementById('draggable');
+const draggable = document.getElementById("draggable");
 
-let offsetX, offsetY, isDragging = false;
+let offsetX,
+  offsetY,
+  isDragging = false;
 
-draggable.addEventListener('mousedown', (e) => {
+draggable.addEventListener("mousedown", (e) => {
   isDragging = true;
   offsetX = e.clientX - draggable.getBoundingClientRect().left;
   offsetY = e.clientY - draggable.getBoundingClientRect().top;
 });
 
-document.addEventListener('mousemove', (e) => {
+document.addEventListener("mousemove", (e) => {
   if (isDragging) {
     draggable.style.left = `${e.clientX - offsetX}px`;
     draggable.style.top = `${e.clientY - offsetY}px`;
   }
 });
 
-document.addEventListener('mouseup', () => {
+document.addEventListener("mouseup", () => {
   isDragging = false;
 });
 
 
-
-
-
-
-
-
-
-
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1,
-    spaceBetween: 15,
-    loop: true,
-    breakpoints: {
-        768: {
-            slidesPerView: 2, 
-            spaceBetween: 20,
-        },
-        1024: {
-            slidesPerView: 1,
-        }
-    }
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 15,
+  loop: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 1,
+    },
+  },
 });
